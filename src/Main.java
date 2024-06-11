653e5d0c7c9f4d8df3135c783602713c9016dab5
Gustavo Ridgue Barbosa de Oliveira <93847986+GustavoRidgue@users.noreply.github.com>
2024-06-10 21:05:15 -0300

    Ajustes no código e add comentários

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,78 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String> palavrasChaves = new HashMap<>();
-        popularPalavrasChave(palavrasChaves);
 
+        // Map que armazena pares chave-valor para saudações
+        Map<String,String> palavrasChaves = popularPalavrasChave();
+        // Map que armazena pares chave-valor da data e hora
+        List<String> palavrasChaveData = Arrays.asList("data", "data e hora", "tempo");
+        // Map que armazena pares chave-valor para despedidas
+        List<String> palavrasChaveDespedida = Arrays.asList("sair", "tchau", "até");
+
+        // Inicializar Scanner para ler o que for digitado no console
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Qual seu nome?");
         String nome = scan.nextLine();
-        System.out.println("Olá " + nome + " o que deseja perguntar?");
 
-        boolean continua = true;
-        while(continua) {
+        System.out.println("Ola " + nome + " o que deseja perguntar?");
+
+        // While para iniciar um loop infinito com a conversa do ChatBot até que saia do loop
+        while(true) {
             String resp = scan.nextLine();
 
-            if (resp.equals("sair")) {
-                System.out.println("Até mais!");
-                continua = false;
-            } else if (Arrays.asList("data", "hora", "data e hora").contains(resp)) {
-                LocalDateTime agora = LocalDateTime.now();
+            // Verifica se usuário digitou palavra chave de despedida para encerrar o programa
+            if (palavrasChaveDespedida.contains(resp.toLowerCase())) {
+                System.out.println("Ate mais!");
+                break;
 
-                DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu HH:mm:ss");
-                String dataFormatada = formatterData.format(agora);
+            // Verifica se usuário digitou palavra chave de data para pegar a data atual
+            } else if (palavrasChaveData.contains(resp.toLowerCase())) {
+                System.out.println("A data autal e " + dataAtualFormatada());
 
-                System.out.println("A data autal é " + dataFormatada);
-            } else if (palavrasChaves.containsKey(resp)) {
-                System.out.println(palavrasChaves.get(resp));
+            // Verifica se usuário digitou palavra chave
+            } else if (palavrasChaves.containsKey(resp.toLowerCase())) {
+                System.out.println(palavrasChaves.get(resp.toLowerCase()));
+
+            // Caso não seja encontrado nenhuma palavra chave, perguntar novamente
             } else {
-                System.out.println("Não entendi o que quis dizer, poderia por favor reformular a pergunta");
+                System.out.println("Nao entendi o que quis dizer, poderia por favor reformular a pergunta");
             }
         }
     }
 
-    public static void popularPalavrasChave(Map<String, String> palavras) {
-        palavras.put("oi", "Olá, tudo bem?");
-        palavras.put("ola", "Olá, tudo bem?");
-        palavras.put("tempo", "O tempo está nublado");
-        palavras.put("tchau", "a data e hora de hoje é ...");
+    // Método para retornar Map palavras chaves e suas respectivas respostas
+    public static Map<String, String> popularPalavrasChave() {
+        Map<String, String> palavras = new HashMap<>();
+
+        palavras.put("oi", "Ola, tudo bem?");
+        palavras.put("ola", "Ola, tudo bem?");
+        palavras.put("opa", "Ola, tudo bem?");
+        palavras.put("clima", "O tempo esta nublado");
+        palavras.put("pais", "Este codigo foi criado no Brasil");
+        palavras.put("piada", "Por que os quimicos sao otimos em resolver problemas? Porque eles tem todas as solucoes!");
+        palavras.put("teste", "Teste funcionando");
+        palavras.put("futebol", "Brasil e o pais do futebol");
+        palavras.put("uam", "Universidade Anhembi Morumbi");
+        palavras.put("bom dia", "Bom dia");
+        palavras.put("boa tarde", "Boa tarde");
+        palavras.put("boa noite", "Boa noite");
+        palavras.put("grupo", "Grupo B08");
+
+        return palavras;
     }
 
+    // Método para pegar a data e hora atual
+    public static String dataAtualFormatada() {
+        LocalDateTime agora = LocalDateTime.now();
+        DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu HH:mm:ss");
+
+        return formatterData.format(agora);
+    }
 }
